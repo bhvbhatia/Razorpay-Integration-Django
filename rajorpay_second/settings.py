d93883df839efd06d46d8539979a68cf493afe96
arvy <arvy@example.com>
2021-05-19 13:11:53 +0530

    djstatic

--- rajorpay_second/settings.py
+++ rajorpay_second/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
-STATIC_URL = os.path.join(BASE_DIR,"static")
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -124,6 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
-STATICFILES_DIRS = [STATIC_URL,]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
