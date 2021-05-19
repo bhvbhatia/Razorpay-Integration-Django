f70cdab95806e1f4359ae873d8f27cd09cf34c30
arvy <arvy@example.com>
2021-05-19 14:11:11 +0530

    static

--- rajorpay_second/settings.py
+++ rajorpay_second/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 
@@ -125,8 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = [STATIC_ROOT,]
